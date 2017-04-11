218ebecbe491341aba5c6a8db21bf7e4281a463f
Rakesh Patel <emailme@rakeshrpatel.com>
2017-04-11 08:59:18 -0400

    basic app updated

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,16 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 
-angular.module('todo', ['ionic']);
+angular.module('todo', ['ionic'])
+
+.controller('TodoCtrl', function($scope) {
+  $scope.tasks = [
+    { title: 'Collect coins' },
+    { title: 'Eat mushrooms' },
+    { title: 'Get high enough to grab the flag' },
+    { title: 'Find the Princess' }
+  ];
+});
 
 angular.module('starter', ['ionic'])
 
